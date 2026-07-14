66ea8a3bc3cbb355d29d5f74d6dd8574c415bc17
TAONV <nguyentao95@gmail.com>
2026-07-14 18:13:09 +0700

    up

--- src/app/[lang]/(dashboard)/(private)/docs/overview/page.tsx
+++ src/app/[lang]/(dashboard)/(private)/docs/overview/page.tsx
@@ -1,31 +1,294 @@
 // Component Imports
-import FAQ from '@views/pages/faq'
+import type {ColumnTable} from 'es-grid-template';
+import { TableComponent} from 'es-grid-template'
+import { faker } from '@faker-js/faker';
 
-// Data Imports
-import { getFaqData } from '@/app/server/actions'
+const genderData = (length: number, page: number, pageSize: number) => {
 
-/**
- * ! If you need data using an API call, uncomment the below API code, update the `process.env.API_URL` variable in the
- * ! `.env` file found at root of your project and also update the API endpoints like `/pages/faq` in below example.
- * ! Also, remove the above server action import and the action itself from the `src/app/server/actions.ts` file to clean up unused code
- * ! because we've used the server action for getting our static data.
- */
+  return Array.from({ length }).map<any>((_, index) => ({
+    id: ((page - 1) * pageSize) + index.toString(),
 
-/* const getFaqData = async () => {
-  // Vars
-  const res = await fetch(`${process.env.API_URL}/pages/faq`)
+    // id: newGuid(),
+    // employeefield: faker.number.int({ min: 0, max: 1 }),
+    employeefield: faker.string.alphanumeric(10),
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch faqData')
-  }
+    // productName: `${faker.internet.username()} ${faker.internet.username()} ${faker.internet.username()}`,
+    productName: `${faker.internet.username()}`,
 
-  return res.json()
-} */
+    // unitName: faker.internet.username() + faker.internet.username() + faker.internet.username(),
+    unitName: faker.internet.username(),
+    onBoardingDate: undefined,
+    name: faker.internet.username(),
+
+    // time: '15/05/2026',
+    time: '2026-05-31T00:00:00',
+
+    // birthday: faker.date.birthdate(),
+    note: '',
+    age: faker.number.int({ min: 1000, max: 10000 }),
+    street: faker.location.street(),
+    building: faker.datatype.boolean(),
+    number: index + 1,
+    7: faker.location.streetAddress(true),
+    8: faker.location.streetAddress(true),
+    companyAddress: faker.location.streetAddress(true),
+
+    // companyAddress: '',
+    // companyName: faker.company.buzzAdjective(),
+    companyName: '',
+    gender: 'M',
+
+  }))
+
+}
 
 const OverviewPage = async () => {
-  const data = await getFaqData()
 
-  return <FAQ data={data} />
+    const total = 100
+
+    const pageSize = 20
+
+    const data  = genderData(total, 1, pageSize)
+
+
+  const headerColumns: ColumnTable[] = [
+  {
+    field: '#',
+    headerText: 'STT',
+    width: 80,
+    fixed: 'left',
+
+    // getCellProps
+    headerCellProps: {
+      style: {
+        // backgroundColor: 'red',
+        // border: 0
+      }
+    }
+  },
+
+  // {
+  //   field: 'employeeCode',
+  //   headerText: 'employeeCode',
+  //   width: 150,
+  //   textAlign: 'left',
+  //   // lock: true,
+  //   headerTextAlign: 'center',
+  //   headerTemplate: () => {
+  //     return (
+  //       <div>
+  //         <span>abc</span>
+  //       </div>
+  //     )
+  //   },
+
+  //   // getCellProps(value: any, record: any, rowIndex: number): any {
+  //   //   if (rowIndex === 3) {
+  //   //     return { rowSpan: 2, }
+  //   //   }
+  //   // },
+  //   children: [
+  //     {
+  //       field: 'firstname',
+  //       headerText: 'firstname',
+  //       headerTooltip: 'Họ',
+  //       width: 100,
+  //       // render: () => {
+  //       //   return (
+  //       //     <div style={{ backgroundColor: 'red' }}>
+  //       //       aa
+  //       //     </div>
+  //       //   )
+  //       // },
+
+  //     },
+  //     { field: 'lastname', headerText: 'lastname', width: 150 },
+  //   ]
+  // },
+
+
+  // { field: 'selection_column', headerText: 'selection_column', width: 60 },
+
+  {
+    field: 'productName',
+    headerText: 'productName',
+    width: 500,
+    headerCellProps: {
+      // colSpan: 2,
+      style: {
+        // backgroundColor: 'red',
+        // border: 0
+      }
+    }
+  },
+  { field: 'name', headerText: 'name 1111', visible: false },
+  { field: 'time', headerText: 'time', width: 550, typeFilter: 'DateRange', type: 'date' },
+  { field: 'birthday', headerText: 'birthday', width: 150 },
+  { field: 'age', headerText: 'age 1', width: 150, headerTemplate: (<>Tuổi</>), type: 'number' },
+
+  // ...repeatedColumns,
+  { field: 'street', headerText: 'street', width: 150 },
+  {
+    field: 'companyAddress',
+    headerText: 'companyAddress',
+    width: 550,
+    textAlign: 'right',
+
+    // fixed: 'right'
+    // lock: true,
+  },
+
+  // {
+  //   field: 'command',
+  //   headerText: 'command',
+  //   width: 50,
+  //   textAlign: 'right',
+
+  //   // fixed: 'right',
+  //   // lock: true,
+  //   commandItems
+  // }
+]
+
+  return (
+    <div>
+      <TableComponent
+
+        // t={t}
+        // lang={lang}
+        // locale={locale?.gridTable}
+
+        // useVirtual={{
+        //   horizontal: true,
+        //   vertical: true
+        // }}
+        theme={{
+          theme: 'dark',
+
+          // backgroundColor: '#ffffff',
+          cssVariables: {
+            // "--bgcolor": 'blue',
+            // "--hover-bgcolor": 'pink',
+            // "--hover-color": 'red',
+            // "--header-bgcolor": 'blue'
+            // "--border-colo: 'r": 'red'
+          }
+
+        }}
+
+        title={(<>aaaaaaaaaa</>)}
+        bottom={(<>aaaaaaaaaa</>)}
+
+        fullScreen={false}
+        
+        // toolbarItems={toolbarItems}
+
+        // showHeader={false}
+
+        // loading={true}
+
+        columns={headerColumns}
+        dataSource={data}
+        height={700}
+        minHeight={700}
+        allowResizing={false}
+
+        // contextMenuItems={contextMenuItem}
+        pagination={{
+          total,
+          pageSize,
+
+          // currentPage: currentPage,
+          // onChange(page, pageSize1) {
+          //   setCurrentPage(page)
+          //   setPageSize(pageSize1)
+
+            
+          // },
+        }}
+        wrapSettings={{
+          wrapMode: 'Header'
+        }}
+        commandClick={(args) => {
+          console.log('args', args)
+        }}
+
+        showColumnChoose={false}
+
+        selectionSettings={{
+          type: 'single',
+          getCheckboxProps(record) {
+            return {
+              disabled: record?.age > 15
+            }
+          },
+        }}
+
+        expandable={{
+          defaultExpandAllRows: true,
+          expandIconColumnIndex: 1
+        }}
+
+        rowSelected={(args) => {
+
+          console.log('rowSelected', args)
+
+        }}
+        summary
+
+        format={{
+          thousandSeparator: ','
+        }}
+
+        recordDoubleClick={(args) => {
+          console.log('recordDoubleClick', args)
+        }}
+
+      // useOuterBorder={false}
+      // groupAble
+      // groupColumns={['unitName']}
+      // groupSetting={{
+
+      // }}
+
+      // onFilter={() => {
+
+      // }}
+
+      // onSorter={() => {
+
+      // }}
+
+      // defaultSorter={
+      //   [
+      //     {
+      //       field: 'unitName',
+      //       columnKey: 'unitName',
+      //       order: 'ascend'
+      //     }
+      //   ]
+      // }
+
+      // defaultFilter={
+      //   [
+      //     {
+      //       key: "unitName",
+      //       field: "unitName",
+      //       value: "B",
+      //       predicate: "and",
+      //       operator: "startswith"
+      //     }
+      //   ]
+      // }
+
+      // onRowStyles={{
+      //   backgroundColor: 'red'
+      // }}
+      // sortMultiple={false}
+
+      />
+    </div>
+  )
 }
 
 export default OverviewPage
